30b819079ef776420a45480b0dc0e9fe2ca77a96
wineat <you@example.com>
2022-01-11 20:53:45 -0500

    project complete

---
+++ test/helloworldtest.js
@@ -0,0 +1,9 @@
+const Helloworld = artifacts.require("Helloworld");
+
+contract("Helloworld", async function(){
+    it("should initialize correctly", async function(){
+        let instance  = await Helloworld.deployed();
+        let message = await instance.getMessage();
+        assert(message === "Hello Again");
+    })
+});
